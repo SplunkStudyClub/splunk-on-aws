3a3ca73e71ede63a46d29fb0fb85510d3dd35fd6
Aleem Cummins <aleem@cummins.me>
2021-05-04 10:45:17 +0100

    dev

--- restrict_pem_key.bat
+++ restrict_pem_key.bat
@@ -1,10 +1,13 @@
 REM Written by Aleem Cummins - Splunk Study Club
 REM aleem@cummins.me
 REM https://github.com/SplunkStudyClub/splunk-on-aws
+REM This was tested on Windows 10
+REM Parameter 1: Full path to pem file downloaded from AWS
+REM Parameter 2: Windows user account for the pem file permissions to be restricted to
 REM Example usage:
 
 @echo off
-REM example usage C:\Users\Aleem\Documents\GitHub\splunk-on-aws/restrict_pem_key.bat "D:\AWS_Keys\test\SplunkStudyClub.pem" "aleem"
+REM example usage C:\Users\Aleem\Documents\GitHub\splunk-on-aws\restrict_pem_key.bat "D:\AWS_Keys\test\SplunkStudyClub.pem" "aleem"
 echo Set parameter variables
 Set Key=%1
 Set UserName=%2
